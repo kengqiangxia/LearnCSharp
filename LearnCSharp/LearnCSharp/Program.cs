ebe3a9a2e4eb29d3c62d5dcc124fb06c89249311
kengqiangxia <kengqiangxia@gmail.com>
2017-12-12 16:38:44 +0800

    first

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;// 导入微软提供的命名空间
+
+namespace LearnCSharp // 命名空间，用来组织和管理类
+{
+    class Program // 类
+    {
+        static void Main(string[] args)// Main方法 程序的入口
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
+
